2d786cfe8a5dad5d8bfbb52661380d0dec8617d0
DurgeshTiwari <durgesh.tiw98@gmail.com>
2021-04-13 02:41:41 +0530

    Quizz_app_flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'quizbrain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +27,70 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Widget> checkAns = [];
+
+  void checkAnswer(bool userAnswer) {
+    setState(() {
+      if (questionAns.isFinish() == true) {
+        Alert(
+          context: context,
+          type: AlertType.warning,
+          title: "Question End!!!",
+          desc: "Click below to start again.....",
+          buttons: [
+            DialogButton(
+              child: Text(
+                "OK",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              onPressed: () => Navigator.pop(context),
+              color: Color.fromRGBO(225, 0, 0, 1.0),
+            ),
+          ],
+        ).show();
+        questionAns.reset();
+        checkAns = [];
+      }
+      if (userAnswer == questionAns.getCorrectAns()) {
+        print('Ans is ok');
+        checkAns.add(
+          Icon(
+            Icons.check,
+            color: Colors.green,
+          ),
+        );
+      } else if (userAnswer == questionAns.getCorrectAns()) {
+        print('Ans is ok');
+        print('Ans is ok');
+        checkAns.add(
+          Icon(
+            Icons.check,
+            color: Colors.green,
+          ),
+        );
+      } else {
+        print('Ans is worng');
+        print('Ans is ok');
+        checkAns.add(
+          Icon(
+            Icons.close,
+            color: Colors.red,
+          ),
+        );
+      }
+
+      questionAns.checkQuestionNumber();
+    });
+  }
+
+  // List<String> question = [
+  //   'You can lead a cow down stairs but not up stairs.',
+  //   'Approximately one quarter of human bones are in the feet.',
+  //   'A slug\'s blood is green.'
+  // ];
+  // List<bool> answer = [false, true, true];
+  Quizbrain questionAns = Quizbrain();
+  // int index = 0; tacking this to quizbrain
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +103,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questionAns.getQuestion(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +127,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                checkAnswer(true);
               },
             ),
           ),
@@ -79,12 +145,14 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                checkAnswer(false);
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: checkAns,
+        )
       ],
     );
   }
